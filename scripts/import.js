06ac869eda70eeafb40bffa7fa6d7527eed187e3
Anthony You <62709305+AnthYou@users.noreply.github.com>
2024-10-03 19:27:45 +0200

    Technical Assignment - Part One (#1)
    We've created a script/import.js file to import the updated products with the 20% discount on cameras to Algolia.

---
+++ scripts/import.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const algoliasearch = require('algoliasearch');
+const products = require('../data/products.json');
+
+// Apply a 20% discount on camera products
+const updatedProducts = products.map((product) => {
+  if (product.hierarchicalCategories.lvl0 === 'Cameras & Camcorders') {
+    product.price = Math.floor(product.price * 0.8);
+  }
+
+  return product;
+});
+
+const client = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY
+);
+
+const index = client.initIndex(process.env.ALGOLIA_INDEX);
+
+const processRecords = () => {
+  return index.saveObjects(updatedProducts, {
+    autoGenerateObjectIDIfNotExist: true, // Cette option crée automatiquement un ID si nécessaire
+  });
+};
+
+processRecords()
+  .then(() => console.log('Successfully indexed objects!'))
+  .catch((err) => console.error(err));
