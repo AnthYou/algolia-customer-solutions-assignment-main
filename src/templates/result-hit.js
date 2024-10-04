735eb4661d1a20268d3a04ba4f98c9323c498856
Anthony You <62709305+AnthYou@users.noreply.github.com>
2024-10-04 20:31:14 +0200

    Technical Assignment - Part Two (#2)
    We've added Algolia Insights, and tracked click and conversion (add to cart) events.

--- src/templates/result-hit.js
+++ src/templates/result-hit.js
@@ -1,15 +1,37 @@
-const resultHit = hit => `<a class="result-hit">
-  <div class="result-hit__image-container">
-    <img class="result-hit__image" src="${hit.image}" />
-  </div>
-  <div class="result-hit__details">
-    <h3 class="result-hit__name">${hit._highlightResult.name.value}</h3>
-    <p class="result-hit__price">$${hit.price}</p>
-  </div>
-  <div class="result-hit__controls">
-    <button id="view-item" class="result-hit__view">View</button>
-    <button id="add-to-cart" class="result-hit__cart">Add To Cart</button>
-  </div>
-</a>`;
+const resultHit = (hit, { html, sendEvent }) => {
+  return html`<a class="result-hit">
+    <div class="result-hit__image-container">
+      <img class="result-hit__image" src="${hit.image}" />
+    </div>
+    <div class="result-hit__details">
+      <h3 class="result-hit__name">${hit._highlightResult.name.value}</h3>
+      <p class="result-hit__price">$${hit.price}</p>
+    </div>
+    <div class="result-hit__controls">
+      <button
+        id="view-item"
+        class="result-hit__view"
+        onClick="${(event) => {
+          event.stopPropagation();
+          sendEvent('click', hit, 'Product Clicked');
+        }}"
+      >
+        View
+      </button>
+      <button
+        id="add-to-cart"
+        class="result-hit__cart"
+        onClick="${(event) => {
+          event.stopPropagation();
+          sendEvent('conversion', hit, 'Added To Cart', {
+            eventSubType: 'addToCart',
+          });
+        }}"
+      >
+        Add To Cart
+      </button>
+    </div>
+  </a>`;
+};
 
 export default resultHit;
